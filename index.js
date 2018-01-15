447c907cabca589eea5fcd90c490e04c734b84bf
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-15 16:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine,name) {
   var num = katzDeliLine.length + 1 
-  katzDeliLine.push("Welcome, " + name + ". Your are number " + num + " in line.");
+  katzDeliLine.push("Welcome, " + name + ". You are number " + num + " in line.");
   return num
 }
