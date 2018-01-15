7b41e5d0da0dc30e0daf2f9e55e119d7e2c31206
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-15 16:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
       }
       else
       {
-        line += "," + num + ". " + katzDeliLine[i]
+        line += ", " + num + ". " + katzDeliLine[i]
       }
     }
   }
